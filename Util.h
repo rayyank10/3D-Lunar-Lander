31f7494df8ae04d8ea3eed78f50093829bde15e5
rayyank10 <57589073+rayyank10@users.noreply.github.com>
2020-07-03 15:57:36 -0700

    Add files via upload

---
+++ Util.h
@@ -0,0 +1,14 @@
+#pragma once
+//  Kevin M. Smith - CS 134 SJSU
+
+#include "ofMain.h"
+#include <iostream>
+#include <algorithm>
+#include <vector> 
+bool rayIntersectPlane(const ofVec3f &rayPoint, const ofVec3f &raydir, ofVec3f const &planePoint,
+	const ofVec3f &planeNorm, ofVec3f &point);
+
+ofVec3f reflectVector(const ofVec3f &v, const ofVec3f &normal);
+
+
+
